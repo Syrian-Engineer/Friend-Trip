7c1006294eb665b9ef4e07e149e97f22b5dce7be
Syrian-Engineer <koussaikareem@gmail.com>
2025-01-10 18:42:51 +0300

    Updating Ui

--- src/Layouts/MainPage/Component/Main/Main.tsx
+++ src/Layouts/MainPage/Component/Main/Main.tsx
@@ -1,11 +1,14 @@
+import { useSelector } from "react-redux"
 import Body from "../Body/Body"
 import Header from "../Header/Header"
 import NavBar from "../NavBar/NavBar"
 import "./Main.css"
+import { RootState } from "../../../NewTrip/Component/NewTrip/NewTrip"
 
 const Main = () => {
+  const themePage = useSelector((state:RootState) => state.Theme.ThemePage)
   return (
-    <div className=" main-page   relative flex flex-col gap-16 w-full min-h-[60rem] text-white  ">
+    <div className={` ${themePage?"bg-white text-black":"text-white bg-black"} main-page   relative flex flex-col gap-16 w-full min-h-[60rem] transition duration-300`}>
         <NavBar />
         <Header />
         <Body />
